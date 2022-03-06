c9b0221bd17310704439b7711eefb70e47c01bc6
kukac001 <daniel.kozma13@gmail.com>
2022-03-06 15:08:05 +0100

    new background color

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import MainGrid from './components/MainGrid';
 
 function App() {
   return (
+    
     <div className="App w-full h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black relative overflow-hidden flex">
-      <MainGrid/>
+    <MainGrid/>
     </div>
   );
 }
--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -3,7 +3,7 @@ import Tilt from 'react-parallax-tilt';
 
 const MainGrid = () => {
     return ( 
-        <div className="w-11/12 h-3/5 bg-white bg-opacity-10 grid grid-cols-12 gap-4 text-3xl rounded-3xl border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-md font-mono text-white text-center justify-center content-center mx-auto mt-5 text-2xl">
+        <div className="w-11/12 h-3/5 bg-white bg-opacity-10 grid grid-cols-12 gap-4 rounded-3xl border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-md font-mono text-white text-center justify-center content-center mx-auto mt-5 text-2xl">
             <div className="col-span-3 row-span-6 pt-5">
                 <img src={Dani} alt="Logo" className="w-3/5 mx-auto rounded-3xl"/>
                 <h1 className="mt-5 ">David Daniel Kozma</h1>
