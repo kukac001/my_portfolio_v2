59ebf8ac6f850fbf04f8d6aa397ccc20af824db6
David Daniel Kozma~ <daniel.kozma13@gmail.com>
2022-04-07 20:42:40 +0200

    navbar + ui update

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -5,9 +5,9 @@ const MainGrid = () => {
     return ( 
         <div className="w-11/12 bg-white bg-opacity-10 flex rounded-3xl border-opacity-30 border-r-0 border-b-0 backdrop-filter backdrop-blur-md font-mono text-white mt-5 text-2xl m-auto">
             <div className="flex flex-col justify-center items-center w-max-xs md:flex-row">
-                <img src={Dani} alt="Logo" className="rounded-3xl bg-red-100 w-3/5 mt-5 md:w-2/5 md:mb-5"/>
+                <img src={Dani} alt="Logo" className="rounded-3xl w-3/5 mt-5 md:w-2/5 md:mb-5"/>
                 <h1 className="text-xl bg-slate-400 sm:text-4xl mt-5 mb-5 md:ml-5">David Daniel Kozma</h1>
-                <p className="text-lg">
+                <p className="text-lg ml-5 md:text-5xl">
                     <Typical
                         loop={Infinity}
                         wrapper='b'
