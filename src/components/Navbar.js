446231579bb519ffcc48a6ed62080a331002b7e6
David Daniel Kozma~ <daniel.kozma13@gmail.com>
2022-04-19 12:07:18 +0200

    Profile done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () =>{
     ];
     let [open,setOpen]=useState(false);
   return (
-    <div className='shadow-md w-full top-0 left-0 bg-slate-300'>
+    <div className='shadow-md w-full top-0 left-0 bg-slate-300 text-white'>
       <div className='md:flex items-center justify-between py-4 md:px-10 px-7'>
       <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
       text-gray-800'>
@@ -20,7 +20,7 @@ const Navbar = () =>{
       <ion-icon name={open ? 'close':'menu'}></ion-icon>
       </div>
 
-      <ul className={`mr-6 md:flex md:items-center  bg-slate-400 md:pb-0 pb-12 absolute md:static md:z-auto z-[1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
+      <ul className={`mr-6 md:flex md:items-center  bg-slate-600 md:pb-0 pb-12 absolute md:static md:z-auto z-[1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ':'top-[-490px]'}`}>
         {
           Links.map((link)=>(
             <li key={link.name} className='md:ml-8 text-xl md:my-0 my-7'>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,58 @@
+import { AiFillSetting } from 'react-icons/ai';
+import { AiFillWechat } from 'react-icons/ai';
+import { AiFillSliders } from 'react-icons/ai';
+import { AiFillUnlock } from 'react-icons/ai';
+
 const Profile = () => {
     return ( 
-        <div className="mt-96 text-white" id="profile">
-            <h1>asd</h1>
-            <p>asd</p>
+        <div className='bg-slate-600 mt-10 text-white font-mono text-center'>
+            <h1 className="text-3xl mb-5">A quick introduction</h1>
+        <div className="flex justify-center flex-col sm:flex-row items-center" id="profile">
+            <div className="m-1 h-52 w-10/12 flex flex-col bg-slate-300 text-black rounded-xl items-center shadow-2xl mb-5 sm:w-4/12">
+                <div className="mt-5">
+                    <AiFillSetting size={56}/>
+                </div>
+                <div>
+                    <h1 className="text-xl font-black tracking-widest lg:text-3xl">Efficiency</h1>
+                </div>
+                <div>
+                    <p className="text-sm mt-5 lg:text-lg">Always want to find the most efficient solution for my problems.</p>
+                </div>
+            </div>
+            <div className="m-1 h-52 w-10/12 flex flex-col bg-slate-300 text-black rounded-xl items-center shadow-2xl mb-5 sm:w-4/12">
+            <div className="mt-5">
+                    <AiFillWechat size={56}/>
+                </div>
+                <div>
+                    <h1 className="text-xl font-black tracking-widest lg:text-3xl">Beloved</h1>
+                </div>
+                <div>
+                    <p className="text-sm mt-5 lg:text-lg">Spending quality times with my loved ones. asdasdasdasdasdasd</p>
+                </div>
+            </div>
+            <div className="m-1 h-52 w-10/12 flex flex-col bg-slate-300 text-black rounded-xl items-center shadow-2xl mb-5 sm:w-4/12">
+            <div className="mt-5">
+                    <AiFillSliders size={56}/>
+                </div>
+                <div>
+                    <h1 className="text-xl font-black tracking-widest lg:text-3xl">Investment</h1>
+                </div>
+                <div>
+                    <p className="text-sm mt-5 lg:text-lg">Investing in myself, also in crypto, stocks and commodities.</p>
+                </div>
+            </div>
+            <div className="m-1 h-52 w-10/12 flex flex-col bg-slate-300 text-black rounded-xl items-center shadow-2xl mb-5 sm:w-4/12">
+            <div className="mt-5">
+                    <AiFillUnlock size={56}/>
+                </div>
+                <div>
+                    <h1 className="text-xl font-black tracking-widest lg:text-3xl">Health</h1>
+                </div>
+                <div>
+                    <p className="text-sm mt-5 lg:text-lg">Maintaining my health, both physically and mentally.</p>
+                </div>
+            </div>
+        </div>
         </div>
     );
 }
